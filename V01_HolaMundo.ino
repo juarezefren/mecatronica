b27943ecb91f2ca856427aee8c2160e8917ec4ff
Efrén Juárez-Castillo <46583863+juarezefren@users.noreply.github.com>
2020-03-19 21:38:41 -0600

    Add files via upload
    video1

---
+++ V01_HolaMundo.ino
@@ -0,0 +1,15 @@
+
+//#define PIN_LED 13
+int PIN_LED = 13;
+
+void setup() {
+  pinMode(PIN_LED,OUTPUT);
+}
+
+void loop() {
+ digitalWrite(PIN_LED,HIGH);
+ delay(1000);
+ digitalWrite(PIN_LED,LOW);
+ delay(1000);
+
+}
