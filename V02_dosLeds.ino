a15e7679baf0a32f96a3791a59a03838515b30a4
Efrén Juárez-Castillo <46583863+juarezefren@users.noreply.github.com>
2020-03-24 15:11:45 -0600

    Video02
    Video02

---
+++ V02_dosLeds.ino
@@ -0,0 +1,17 @@
+#define LED1 8
+#define LED2 9
+
+void setup() {
+ pinMode(LED1,OUTPUT);
+ pinMode(LED2,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED1,HIGH);
+  delay(500);
+  digitalWrite(LED1,LOW);
+  digitalWrite(LED2,HIGH);
+  delay(1700);
+  digitalWrite(LED2,LOW);
+  delay(100);
+}
