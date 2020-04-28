ac63b5313290506ebf195c28b5fbdc6b8548bb4f
Efrén Juárez-Castillo <46583863+juarezefren@users.noreply.github.com>
2020-04-28 10:18:23 -0500

    Video 04 Python
    Video 04 Python

---
+++ python-vid04.py
@@ -0,0 +1,8 @@
+# Calcula el área de un triángulo
+
+base = input("Introduce la base :")
+altura = input("Introduce la altura :")
+
+area = float(base) * float(altura) / 2
+
+print("El área del triángulo es: " , area)
